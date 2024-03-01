e384ca6d765d7a9d56456392e996f8c7e235c09d
Yogit Nainani <nainaniyogit@gmail.com>
2024-03-02 04:02:02 +0530

    Navbar create

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,26 @@
 "use client";
 
+import Link from "next/link";
 import { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { RxCross1 } from "react-icons/rx";
+import { motion } from "framer-motion";
 
 export default function Navbar() {
   const [toggle, setToggle] = useState(false);
+  const [hoverX, setHoverX] = useState(0);
+  const [hoverY, setHoverY] = useState(0);
+
   const handleToggle = () => {
     setToggle(!toggle);
+  };
+
+  const handleHover = (e: any) => {
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - (rect.left*1.4);
+    const y = e.clientY - (rect.top*1.4);
+    setHoverX(x);
+    setHoverY(y);
   };
 
   return (
@@ -20,13 +33,75 @@ export default function Navbar() {
           className="h-10 w-10 border-2 border-[#fefffa] text-[#fefffa] text-2xl flex justify-center items-center"
           onClick={handleToggle}
         >
-          <RxHamburgerMenu className={`absolute ${toggle ? "opacity-0" : "opacity-1"} ${toggle ? "rotate-0" : "rotate-[360deg]"} transition duration-500`}/>
-          <RxCross1 className={`absolute ${toggle ? "opacity-1" : "opacity-0"} ${toggle ? "rotate-0" : "rotate-[360deg]"} transition duration-500`} />
+          <RxHamburgerMenu
+            className={`absolute ${toggle ? "opacity-0" : "opacity-1"} ${
+              toggle ? "rotate-0" : "rotate-[360deg]"
+            } transition duration-500`}
+          />
+          <RxCross1
+            className={`absolute ${toggle ? "opacity-1" : "opacity-0"} ${
+              toggle ? "rotate-0" : "rotate-[360deg]"
+            } transition duration-500`}
+          />
         </div>
       </div>
 
-      // special div
-      {toggle && <div className="bg-[#0C0C0F] opacity-95 w-full min-h-screen absolute z-20 transition duration-1000"></div>}
+      {toggle && (
+        <motion.div className="bg-[#0C0C0F] opacity-[99%] w-full min-h-screen absolute z-20 transition duration-1000 flex justify-center items-center">
+          <ul className="">
+            <motion.li onMouseMove={handleHover} className="mb-4">
+              <Link
+                data-text="Home"
+                href={"/"}
+                className="text-8xl leading-none text-[#fefffa] font-anton tracking-widest uppercase self-start transition duration-500 nav-link"
+              >
+                <motion.h1 style={{ x: hoverX, y: hoverY }} className="hollow-bg absolute">Home</motion.h1>
+                Home
+              </Link>
+            </motion.li>
+            <motion.li onMouseMove={handleHover} className="mb-4">
+              <Link
+                data-text="About"
+                href={"/about"}
+                className="text-8xl leading-none text-[#fefffa] font-anton tracking-widest uppercase self-start transition duration-500 nav-link "
+              >
+                <motion.h1 style={{ x: hoverX, y: hoverY }} className="hollow-bg absolute">About</motion.h1>
+                About
+              </Link>
+            </motion.li>
+            <motion.li onMouseMove={handleHover} className="mb-4">
+              <Link
+                data-text="Experience"
+                href={"/experience"}
+                className="text-8xl leading-none text-[#fefffa] font-anton tracking-widest uppercase self-start transition duration-500 nav-link"
+              >
+                <motion.h1 style={{ x: hoverX, y: hoverY }} className="hollow-bg absolute">Experience</motion.h1>
+                Experience
+              </Link>
+            </motion.li>
+            <motion.li onMouseMove={handleHover} className="mb-4">
+              <Link
+                data-text="Projects"
+                href={"/projects"}
+                className="text-8xl leading-none text-[#fefffa] font-anton tracking-widest uppercase self-start transition duration-500 nav-link"
+              >
+                <motion.h1 style={{ x: hoverX, y: hoverY }} className="hollow-bg absolute">Projects</motion.h1>
+                Projects
+              </Link>
+            </motion.li>
+            <motion.li onMouseMove={handleHover} className="mb-4">
+              <Link
+                data-text="Contact"
+                href={"/contact"}
+                className="text-8xl leading-none text-[#fefffa] font-anton tracking-widest uppercase self-start transition duration-500 nav-link"
+              >
+                <motion.h1 style={{ x: hoverX, y: hoverY }} className="hollow-bg absolute">Contact</motion.h1>
+                Contact
+              </Link>
+            </motion.li>
+          </ul>
+        </motion.div>
+      )}
     </div>
   );
 }
