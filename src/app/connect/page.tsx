88e54207f5f3fb374cbd8fd12426dd0ab1a3149c
Yogit Nainani <nainaniyogit@gmail.com>
2024-04-13 23:41:34 +0530

    Minor changes

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -11,23 +11,23 @@ export default function Connect() {
           <form className="flex flex-col w-full gap-2">
             <div className="flex flex-row w-full gap-4">
               <div className="flex flex-col w-1/2">
-                <label htmlFor="fname" className="text-[#fefffa]">First Name:</label>
+                <label htmlFor="fname" className="text-[#fefffa]">First Name</label>
                 <input type="text" className="bg-transparent border border-[#727C57] text-grey-800 p-2 rounded-sm text-[#fefffa] focus:rounded-sm" id="fname" name="fname" placeholder="John" required />
               </div>
 
               <div className="flex flex-col w-1/2">
-                <label htmlFor="lname" className="text-[#fefffa]">Last Name:</label>
+                <label htmlFor="lname" className="text-[#fefffa]">Last Name</label>
                 <input type="text" className="bg-transparent border border-[#727C57] text-grey-800 p-2 rounded-sm text-[#fefffa] focus:rounded-sm" id="lname" name="lname" placeholder="Doe" required />
               </div>
             </div>
 
             <div className="flex flex-col">
-            <label htmlFor="email" className="text-[#fefffa]">Email:</label>
+            <label htmlFor="email" className="text-[#fefffa]">Email</label>
             <input type="email" className="bg-transparent border border-[#727C57] text-grey-800 p-2 rounded-sm text-[#fefffa] focus:rounded-sm" id="email" name="email" placeholder="johndoe@email.com" required />
             </div>
             
             <div className="flex flex-col">
-            <label htmlFor="message" className="text-[#fefffa]">Your Message:</label>
+            <label htmlFor="message" className="text-[#fefffa]">Your Message</label>
             <textarea id="message" className="bg-transparent border border-[#727C57] text-grey-800 p-2 rounded-sm text-[#fefffa] focus:rounded-sm" name="message" placeholder="Hi! I would like to connect with you." required></textarea>
             </div>
 
